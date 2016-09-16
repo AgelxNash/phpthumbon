c85b9a9a77f7be250cafc00ce81677ab6f321f40
gvozdb <i@owl.kz>
2016-09-16 19:40:23 +0500

    1.3.2-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -2,20 +2,17 @@
 /**
  * phpThumbOn
  * Создание превьюх картинок
- *
  * Copyright 2013 by Agel_Nash <Agel_Nash@xaker.ru>
- *
  * @category images
- * @license GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
- * @author Agel_Nash <Agel_Nash@xaker.ru>
- *
+ * @license  GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
+ * @author   Agel_Nash <Agel_Nash@xaker.ru>
  */
 /**
- * @package phpThumbOn
+ * @package    phpThumbOn
  * @subpackage build
  */
 
-define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))).'/');
+define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
@@ -29,8 +26,8 @@ define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
 define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
 
 /* define package */
-define('PKG_NAME','phpThumbOn');
-define('PKG_NAME_LOWER',strtolower(PKG_NAME));
+define('PKG_NAME', 'phpThumbOn');
+define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '1.3.1');
-define('PKG_RELEASE','pl');
+define('PKG_VERSION', '1.3.2');
+define('PKG_RELEASE', 'pl');
