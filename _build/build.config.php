373a6863e1aae55dfc38f1655956365ec72ad322
gvozdb <pavelgvozdb@yandex.ru>
2019-02-11 14:57:59 +0600

    1.3.5-beta

--- _build/build.config.php
+++ _build/build.config.php
@@ -29,5 +29,5 @@ define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
 define('PKG_NAME', 'phpThumbOn');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '1.3.4');
+define('PKG_VERSION', '1.3.5');
 define('PKG_RELEASE', 'beta');
